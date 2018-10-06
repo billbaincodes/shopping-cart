d80acaf2e1d834998d9b2a8220419d9ab44ffca3
bill <williamandrewbain@gmail.com>
2018-10-06 13:15:02 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CartHeader from './CartHeader.js'
 
-class App extends Component {
+class CartHeader extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <nav className="navbar navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">Shopping Cart</a>
+      </nav>
     );
   }
 }
 
-export default App;
+
+
+
+export default CartHeader;
+
---
+++ src/CartFooter.js
@@ -0,0 +1,11 @@
+
+
+class CartFooter extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">Shopping Cart</a>
+      </nav>
+    );
+  }
+}
